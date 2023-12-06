c1228a3bc2efec056b8f28bc4ba30b1cda2db422
videodreaming <robin@soundself.com>
2023-12-06 08:48:31 -0600

    Auto stash before merge of "ReefImitoneWork" and "origin/ReefImitoneWork"

--- Assets/DevModeScript+Assets/Scripts/DevModeSettings.cs
+++ Assets/DevModeScript+Assets/Scripts/DevModeSettings.cs
@@ -19,6 +19,7 @@ public class DevModeSettings : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Alpha0)){
             devMode = false;
             forceToneActive = false;
+            forceNoTone = false;
         }
         if(Input.GetKeyDown(KeyCode.Alpha9)){
             if(startInDevMode){
@@ -26,27 +27,22 @@ public class DevModeSettings : MonoBehaviour
             }
         }
         if(devMode == true){
-            if(Input.GetKey(KeyCode.LeftShift)){
-                if(Input.GetKey(KeyCode.T)){
-                    forceNoTone = true;
-                    forceToneActive = false;
-                } 
-                else 
-                {
-                    forceNoTone = false;
-                }
+            //SHIFT+T = FORCE TONEACTIVE 
+            if(Input.GetKeyUp(KeyCode.LeftShift)){ //Release Control
+                forceToneActive = false;
+                forceNoTone = false;
             }
-            else 
-            {
+            else if(Input.GetKey(KeyCode.LeftShift)){
                 if(Input.GetKey(KeyCode.T)){
                     forceToneActive = true;
                     forceNoTone = false;
-                } else {
-                forceToneActive = false;
+                } 
+                else
+                {
+                    forceToneActive = false;
+                    forceNoTone = true;
                 }
             }
-
-            
         }
     }
 }
