a75b0fba5fe9fd21acf98a162f098e2b5f7f55b9
Lorna Dune <ljkrier@gmail.com>
2024-02-27 15:29:54 -0600

    Added GameCalls to set switches (Long/Short)

--- SoundSelfUnityMacGame_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ SoundSelfUnityMacGame_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -19,6 +19,25 @@ namespace AK
         static const AkUniqueID PLAY_VO_OPENING = 1386597262U;
     } // namespace EVENTS
 
+    namespace STATES
+    {
+        namespace GAMEMODE
+        {
+            static const AkUniqueID GROUP = 261089142U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ADJUNCT = 2534402132U;
+                static const AkUniqueID INTEGRATION = 4094247797U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PASSIVE = 2595709632U;
+                static const AkUniqueID PREPARATION = 3147549296U;
+                static const AkUniqueID WISDOM = 4033993124U;
+            } // namespace STATE
+        } // namespace GAMEMODE
+
+    } // namespace STATES
+
     namespace SWITCHES
     {
         namespace INTERACTIVEMUSICSWITCHGROUP
@@ -45,6 +64,20 @@ namespace AK
             } // namespace SWITCH
         } // namespace LINEARMUSIC_OPENING
 
+        namespace VO_CLOSINGGOODBYE
+        {
+            static const AkUniqueID GROUP = 1815817025U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ANCHORING = 1927133646U;
+                static const AkUniqueID GRATITUDE = 1721781634U;
+                static const AkUniqueID LONG = 674228435U;
+                static const AkUniqueID SHORT = 2585211341U;
+                static const AkUniqueID SILENCE = 3041563226U;
+            } // namespace SWITCH
+        } // namespace VO_CLOSINGGOODBYE
+
         namespace VO_OPENING
         {
             static const AkUniqueID GROUP = 4212583561U;
@@ -57,6 +90,74 @@ namespace AK
             } // namespace SWITCH
         } // namespace VO_OPENING
 
+        namespace VO_POSTURE
+        {
+            static const AkUniqueID GROUP = 184547165U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LIEDOWN = 3891673577U;
+                static const AkUniqueID RELAX = 3551080421U;
+            } // namespace SWITCH
+        } // namespace VO_POSTURE
+
+        namespace VO_SOMATIC
+        {
+            static const AkUniqueID GROUP = 3813349969U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LONG = 674228435U;
+                static const AkUniqueID SHORT = 2585211341U;
+            } // namespace SWITCH
+        } // namespace VO_SOMATIC
+
+        namespace VO_THEMATICCONTENT
+        {
+            static const AkUniqueID GROUP = 3925841397U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIEWELL = 2721896533U;
+                static const AkUniqueID NARRATIVE = 1047870521U;
+                static const AkUniqueID PEACE = 103389341U;
+                static const AkUniqueID SURRENDER = 2965941853U;
+            } // namespace SWITCH
+        } // namespace VO_THEMATICCONTENT
+
+        namespace VO_THEMATICSAVASANA
+        {
+            static const AkUniqueID GROUP = 4155508698U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ANCHORING = 1927133646U;
+                static const AkUniqueID FIREFLIES = 1126065684U;
+                static const AkUniqueID GRATITUDE = 1721781634U;
+                static const AkUniqueID KINDNESS = 2655751454U;
+                static const AkUniqueID LONG = 674228435U;
+                static const AkUniqueID METTA = 2692078280U;
+                static const AkUniqueID NARRATIVE = 1047870521U;
+                static const AkUniqueID PEACE = 103389341U;
+                static const AkUniqueID SHORT = 2585211341U;
+                static const AkUniqueID SILENCE = 3041563226U;
+                static const AkUniqueID SURRENDER = 2965941853U;
+            } // namespace SWITCH
+        } // namespace VO_THEMATICSAVASANA
+
+        namespace VO_WISDOM
+        {
+            static const AkUniqueID GROUP = 683317262U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIEWELL = 2721896533U;
+                static const AkUniqueID JAGUAR = 1398814679U;
+                static const AkUniqueID NOTKNOWING = 1057915111U;
+                static const AkUniqueID PERU = 1743886715U;
+            } // namespace SWITCH
+        } // namespace VO_WISDOM
+
     } // namespace SWITCHES
 
     namespace GAME_PARAMETERS
@@ -68,6 +169,7 @@ namespace AK
         static const AkUniqueID HARMONYSILENTVOLUME = 1111564044U;
         static const AkUniqueID INTERACTIVEMUSICSILENTLOOPS = 2108330916U;
         static const AkUniqueID REDSINEVOLUME = 1546458207U;
+        static const AkUniqueID VO_BUS_VOLUME = 3687478936U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -80,6 +182,7 @@ namespace AK
     {
         static const AkUniqueID AVS_SYSTEM = 65973818U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID VO_BUS = 2018813558U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
