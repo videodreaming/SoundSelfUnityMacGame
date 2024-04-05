6e57266ae3b34a779ea5f47ccf190ae0149ccf2f
Reef <deyingliew@gmail.com>
2024-04-05 15:52:56 +0800

    Env1 and Env2 Sliders

--- Assets/Scripts/UIScripts/LornaDebugs/SliderControllers.cs
+++ Assets/Scripts/UIScripts/LornaDebugs/SliderControllers.cs
@@ -13,6 +13,18 @@ public class SliderControllers : MonoBehaviour
     [SerializeField] private TextMeshProUGUI VOBusText = null;
     [SerializeField] private TextMeshProUGUI silentInteractiveBusText = null;
 
+    [SerializeField] private TextMeshProUGUI env1WaveformText = null;
+    [SerializeField] private TextMeshProUGUI env1RateText = null;
+    [SerializeField] private TextMeshProUGUI env1PWMDutyCycleText = null;
+    [SerializeField] private TextMeshProUGUI env1SmoothingText = null;
+    [SerializeField] private TextMeshProUGUI env1DepthText = null;
+
+     [SerializeField] private TextMeshProUGUI env2WaveformText = null;
+    [SerializeField] private TextMeshProUGUI env2RateText = null;
+    [SerializeField] private TextMeshProUGUI env2PWMDutyCycleText = null;
+    [SerializeField] private TextMeshProUGUI env2SmoothingText = null;
+    [SerializeField] private TextMeshProUGUI env2DepthText = null;
+
     // Update is called once per frame
     public void SliderChange(float value)
     {
@@ -68,6 +80,74 @@ public class SliderControllers : MonoBehaviour
         AkSoundEngine.SetRTPCValue("Master_Volume", localValue);
         masterSliderText.text = localValue.ToString("0");
     }
+
+    public void env1WaveformSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env1_Waveform", localValue);
+        env1WaveformText.text = localValue.ToString("0");
+    }
+    public void env1RateSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env1_Rate", localValue);
+        env1RateText.text = localValue.ToString("0");
+    }
+
+    public void env1PWNDutyCycleSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env1_PWM_DutyCycle", localValue);
+        env1PWMDutyCycleText.text = localValue.ToString("0");
+    }
+
+        public void env1SmoothingSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env1_Smoothing", localValue);
+        env1SmoothingText.text = localValue.ToString("0");
+    }
+
+    public void env1DepthSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env1_Depth", localValue);
+        env1DepthText.text = localValue.ToString("0");
+    }
+
+    public void env2WaveformSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env2_Waveform", localValue);
+        env2WaveformText.text = localValue.ToString("0");
+    }
+
+    public void env2RateSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env2_Rate", localValue);
+        env2RateText.text = localValue.ToString("0");
+    }
+
+    public void env2PWMDutyCycleSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env2_PWM_DutyCycle", localValue);
+        env2PWMDutyCycleText.text = localValue.ToString("0");
+    }
+        public void env2SmoothingSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env2_Smoothing", localValue);
+        env2SmoothingText.text = localValue.ToString("0");
+    }
+    public void env2DepthSlider(float value)
+    {
+        float localValue = value;
+        AkSoundEngine.SetRTPCValue("Env2_Depth", localValue);
+        env2DepthText.text = localValue.ToString("0");
+    }
+
 
 
 }
