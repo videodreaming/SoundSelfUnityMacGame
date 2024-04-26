8e904c11ff2c6a37c0cae74510a14c4d312362ea
Reef <deyingliew@gmail.com>
2024-04-26 15:22:43 -0400

    Auto stash before merge of "ReefImitoneWork" and "origin/ReefImitoneWork"

--- Assets/Scripts/WwiseManagers/WwiseInteractiveMusicManager.cs
+++ Assets/Scripts/WwiseManagers/WwiseInteractiveMusicManager.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WwiseInteractiveMusicManager: MonoBehaviour
+public class WwiseInteractiveMusicManager : MonoBehaviour
 {
     public string currentSwitchState = "B";
     public string currentToningState = "None";
     public float InteractiveMusicSilentLoopsRTPC = 0.0f;
     public float HarmonySilentVolumeRTPC = 0.0f;
     public float FundamentalSilentVolumeRTPC = 0.0f;
-    
+    public ImitoneVoiceIntepreter imitoneVoiceIntepreter;
+    public uint playingId;
+    private bool toneActiveTriggered = false; // Flag to control the event triggering
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +21,38 @@ public class WwiseInteractiveMusicManager: MonoBehaviour
         AkSoundEngine.SetRTPCValue("FundamentalSilentVolume", 30.0f, gameObject);
         AkSoundEngine.PostEvent("Play_InteractiveMusicSystem_SilentLoops", gameObject);
     }
-    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imitoneVoiceIntepreter.toneActiveConfident)
+        {
+            if (!toneActiveTriggered)
+            {
+                toning();
+                toneActiveTriggered = true; // Set the flag to true after the event is triggered
+            }
+        }
+        else
+        {
+            if (toneActiveTriggered)
+            {
+                AkSoundEngine.StopPlayingID(playingId);
+                toneActiveTriggered = false; // Reset the flag when toneActiveConfident becomes false
+            }
+        }
+    }
+
+    void toning()
+    {
+        playingId = AkSoundEngine.PostEvent("Play_InteractiveMusicSystem_Toning", gameObject);
+    }
+
     public void ChangeToningState()
     {
         AkSoundEngine.SetState("SoundWorldMode", currentToningState);
     }
+
     public void ChangeSwitchState()
     {
         AkSoundEngine.SetSwitch("InteractiveMusicSwitchGroup", currentSwitchState, gameObject);
@@ -39,10 +69,12 @@ public class WwiseInteractiveMusicManager: MonoBehaviour
     {
         AkSoundEngine.SetRTPCValue("HarmonySilentVolume", newHarmonyVolumeRTPC, gameObject);
     }
+
     public void setFundamentalSilentVolumeRTPCValue(float newFundamentalVolumeRTPC)
     {
         AkSoundEngine.SetRTPCValue("FundamentalSilentVolume", newFundamentalVolumeRTPC, gameObject);
     }
+
     public void setInteractiveMusicSilentLoopsRTPCValue(float newInteractiveMusicSilentLoopRTPC)
     {
         AkSoundEngine.SetRTPCValue("InteractiveMusicSilentLoops", newInteractiveMusicSilentLoopRTPC, gameObject);
